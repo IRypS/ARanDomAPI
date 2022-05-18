a1dbcef70c7cbc5051b445c47b309982da5a06a0
IRypS <pelaezricardo1502@gmail.com>
2022-05-18 17:00:33 -0500

    API base

---
+++ index.js
@@ -0,0 +1,36 @@
+/**
+ * REQUIRES
+ */
+
+require('dotenv').config();
+
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const dbConnect = require('./config/mongo');
+
+/**
+ * CONFIGURATIONS
+ */
+app.use(cors());
+app.use(express.json()); //? Reemplaza al body parser
+//app.use(express.static("storage"));
+
+const port = process.env.PORT || 3000;
+
+/**
+ * ROUTES
+ */
+//app.use('/api', require('./routes'));
+
+/**
+ * LISTEN PORT
+ */
+app.listen(port, () => {
+  console.log(`Servidor funcionando en el puerto ${port} \n${process.env.URL_PUBLIC}`)
+});
+
+/**
+ * DB CONNECTION
+ */
+dbConnect();
